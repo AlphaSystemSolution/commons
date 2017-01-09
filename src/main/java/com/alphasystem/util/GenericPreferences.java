3acee5b3ccd5209ebb5b2b2e170619b8b40a5357
sfali23 <f.syed.ali@gmail.com>
2017-01-09 08:48:34 -0500

    Added method to get instance with particular type

--- src/main/java/com/alphasystem/util/GenericPreferences.java
+++ src/main/java/com/alphasystem/util/GenericPreferences.java
@@ -2,132 +2,147 @@ package com.alphasystem.util;
 
 import com.alphasystem.BusinessException;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ServiceLoader;
 import java.util.prefs.Preferences;
 
 public abstract class GenericPreferences {
 
-	private static GenericPreferences instance;
+    private static final Map<String, GenericPreferences> instances = Collections.synchronizedMap(new HashMap<>());
+    private static GenericPreferences instance;
 
-	// ~ Instance/static variables .............................................
+    // ~ Instance/static variables .............................................
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @param name
-	 *            DOCUMENT ME!
-	 * @return DOCUMENT ME!
-	 * @throws BusinessException
-	 *             DOCUMENT ME!
-	 */
-	@SuppressWarnings("rawtypes")
-	protected static Class getClassForName(String name)
-			throws BusinessException {
-		try {
-			return Class.forName(name);
-		} catch (Exception ex) {
-			throw new BusinessException(ex.getMessage(), ex);
-		}
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @param name DOCUMENT ME!
+     * @return DOCUMENT ME!
+     * @throws BusinessException DOCUMENT ME!
+     */
+    @SuppressWarnings("rawtypes")
+    protected static Class getClassForName(String name)
+            throws BusinessException {
+        try {
+            return Class.forName(name);
+        } catch (Exception ex) {
+            throw new BusinessException(ex.getMessage(), ex);
+        }
+    }
 
-	/**
-	 * @return the instance
-	 */
-	public static synchronized GenericPreferences getInstance() {
-		if (instance == null) {
-			ServiceLoader<GenericPreferences> serviceLoader = ServiceLoader
-					.load(GenericPreferences.class);
-			for (GenericPreferences pref : serviceLoader) {
-				instance = pref;
-				if (instance != null) {
-					break;
-				}
-			}
-			if(instance==null){
-				System.err.println("Still null");
-			}
-		}
-		return instance;
-	}
+    /**
+     * @return the instance
+     */
+    public static synchronized GenericPreferences getInstance() {
+        if (instance == null) {
+            ServiceLoader<GenericPreferences> serviceLoader = ServiceLoader
+                    .load(GenericPreferences.class);
+            for (GenericPreferences pref : serviceLoader) {
+                instance = pref;
+                if (instance != null) {
+                    break;
+                }
+            }
+            if (instance == null) {
+                System.err.println("Still null");
+            }
+        }
+        return instance;
+    }
 
-	// ~ Constructors ..........................................................
+    @SuppressWarnings({"unchecked"})
+    public static <P extends GenericPreferences> P getInstance(Class<P> _class) {
+        final String _className = _class.getName();
+        P instance = (P) instances.get(_className);
+        if (instance == null) {
+            ServiceLoader<GenericPreferences> serviceLoader = ServiceLoader
+                    .load(GenericPreferences.class);
 
-	protected Preferences root;
+            for (GenericPreferences pref : serviceLoader) {
+                if (_className.equals(pref.getClass().getName())) {
+                    instance = (P) pref;
+                    instances.put(_className, instance);
+                    break;
+                }
+            }
+        }
+        if (instance == null) {
+            throw new RuntimeException(String.format("Unable to find instance for Class \"%s\".", _className));
+        }
+        return instance;
+    }
 
-	// ~ Methods ...............................................................
+    // ~ Constructors ..........................................................
 
-	/**
-	 * Creates a new GenericPreferences object.
-	 * 
-	 * @param c
-	 *            DOCUMENT ME!
-	 */
-	protected GenericPreferences(Class<?> c) {
-		root = Preferences.userNodeForPackage(c);
-	}
+    private Preferences root;
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @param nodeName
-	 *            DOCUMENT ME!
-	 * @param key
-	 *            DOCUMENT ME!
-	 * @param def
-	 *            DOCUMENT ME!
-	 * @return DOCUMENT ME!
-	 */
-	public String get(String nodeName, String key, String def) {
-		Preferences node = root.node(nodeName);
-		return node.get(key, def);
-	}
+    // ~ Methods ...............................................................
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @param nodeName
-	 *            DOCUMENT ME!
-	 * @return DOCUMENT ME!
-	 */
-	public Preferences getNode(String nodeName) {
-		return root.node(nodeName);
-	}
+    /**
+     * Creates a new GenericPreferences object.
+     *
+     * @param c DOCUMENT ME!
+     */
+    protected GenericPreferences(Class<?> c) {
+        root = Preferences.userNodeForPackage(c);
+    }
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @return DOCUMENT ME!
-	 */
-	public Preferences getRoot() {
-		return root;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @param nodeName DOCUMENT ME!
+     * @param key      DOCUMENT ME!
+     * @param def      DOCUMENT ME!
+     * @return DOCUMENT ME!
+     */
+    public String get(String nodeName, String key, String def) {
+        Preferences node = root.node(nodeName);
+        return node.get(key, def);
+    }
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @param nodeName
-	 *            DOCUMENT ME!
-	 * @param key
-	 *            DOCUMENT ME!
-	 * @param value
-	 *            DOCUMENT ME!
-	 */
-	public void put(String nodeName, String key, String value) {
-		Preferences node = root.node(nodeName);
-		node.put(key, value);
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @param nodeName DOCUMENT ME!
+     * @return DOCUMENT ME!
+     */
+    public Preferences getNode(String nodeName) {
+        return root.node(nodeName);
+    }
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @throws BusinessException
-	 *             DOCUMENT ME!
-	 */
-	public void save() throws BusinessException {
-		try {
-			root.flush();
-		} catch (Exception ex) {
-			throw new BusinessException(ex.getMessage(), ex);
-		}
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public Preferences getRoot() {
+        return root;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param nodeName DOCUMENT ME!
+     * @param key      DOCUMENT ME!
+     * @param value    DOCUMENT ME!
+     */
+    public void put(String nodeName, String key, String value) {
+        Preferences node = root.node(nodeName);
+        node.put(key, value);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws BusinessException DOCUMENT ME!
+     */
+    public void save() throws BusinessException {
+        try {
+            root.flush();
+        } catch (Exception ex) {
+            throw new BusinessException(ex.getMessage(), ex);
+        }
+    }
 }
